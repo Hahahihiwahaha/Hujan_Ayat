d7a84fcc6c097f93eba22e49c144e29afc2753b7
Mufliha Safira <muflihasafira@gmail.com>
2020-10-18 14:49:58 +0700

    a

--- app/src/main/java/com/example/hujan_ayat/List.java
+++ app/src/main/java/com/example/hujan_ayat/List.java
@@ -9,28 +9,30 @@ import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class List extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_list);
+
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
 
-        bottomNavigationView.setSelectedItemId(R.id.library);
+        bottomNavigationView.setSelectedItemId(R.id.list);
 
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
                 switch (menuItem.getItemId()){
                     case R.id.home:
+                        startActivity(new Intent(getApplicationContext(),
+                                MainActivity.class));
+                        overridePendingTransition(0,0);
                         return true;
 
                     case R.id.list:
-                        startActivity(new Intent(getApplicationContext(),
-                                List.class));
-                        overridePendingTransition(0,0);
+
                         return true;
 
                     case R.id.settings:
--- app/src/main/java/com/example/hujan_ayat/Settings.java
+++ app/src/main/java/com/example/hujan_ayat/Settings.java
@@ -9,22 +9,26 @@ import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class Settings extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_settings);
+
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
 
-        bottomNavigationView.setSelectedItemId(R.id.library);
+        bottomNavigationView.setSelectedItemId(R.id.settings);
 
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
                 switch (menuItem.getItemId()){
                     case R.id.home:
+                        startActivity(new Intent(getApplicationContext(),
+                                MainActivity.class));
+                        overridePendingTransition(0,0);
                         return true;
 
                     case R.id.list:
@@ -34,9 +38,7 @@ public class MainActivity extends AppCompatActivity {
                         return true;
 
                     case R.id.settings:
-                        startActivity(new Intent(getApplicationContext(),
-                                Settings.class));
-                        overridePendingTransition(0,0);
+
                         return true;
                 }
 
